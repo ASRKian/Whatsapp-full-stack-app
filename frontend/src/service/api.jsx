186957bbb40b8169fe0d30b4e5ef629695cbd731
Lokendra gupta <guptalokendra18@gmail.com>
2025-12-04 15:57:03 +0530

    backend url change

--- frontend/src/service/api.jsx
+++ frontend/src/service/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = 'http://localhost:8000'
+const url = 'https://whatsapp-full-stack-app-backend.onrender.com'
 export const addUser = async (data) => {
     try {
         await axios.post(`${url}/add`, data)
